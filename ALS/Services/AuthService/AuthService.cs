aa55eae88890a580defd7aea6b956acc5db156d0
NothingIsGood <nothingisgood@yandex.ru>
2019-05-17 22:05:23 +0400

    Authorize with roles now should work(??)

--- ALS/Services/AuthService/AuthService.cs
+++ ALS/Services/AuthService/AuthService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using ALS.EntityСontext;
 using CryptoHelper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
@@ -24,8 +26,22 @@ namespace ALS.Services.AuthService
         {
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.Name, $"{user.Surname} {user.Name}")
             };
+
+            using (var db = new ApplicationContext())
+            {
+                var dbUser = db.Users.Include(u => u.UserRoles).ThenInclude(ur => ur.Role).FirstOrDefault(u => u.Email == email);
+                if (dbUser != null)
+                {
+                    foreach (var role in dbUser.UserRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, role.Role.RoleName.ToString()));
+                    }
+                }
+            }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
