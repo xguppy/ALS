7a01a747acde5c664a4480f9fd318a540c1f495d
Guppy <xguppy@yandex.ru>
2019-05-22 23:18:26 +0400

    Laboratory work controller and DTO

---
+++ ALS/DTO/LaboratoryWorkDTO.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using ALS.EntityСontext;
+
+namespace ALS.DTO
+{
+    public class LaboratoryWorkDTO
+    {
+        public int TemplateLaboratoryWorkId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Evaluation Evaluation { get; set; }
+        public string Cipher { get; set; }
+        public int UserId { get; set; }
+        [Column(TypeName = "jsonb")]
+        public string Constraints { get; set; }
+    }
+}
