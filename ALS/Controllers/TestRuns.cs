ed4ec16b8a4ab79f8d5918e982e83d4aa26411aa
Guppy <xguppy@yandex.ru>
2019-05-31 22:08:14 +0400

    Crud testRun

---
+++ ALS/Controllers/TestRuns.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Threading.Tasks;
+using ALS.EntityСontext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALS.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Teacher")]
+    public class TestRuns : Controller
+    {
+        private readonly ApplicationContext _db;
+                
+        public TestRuns(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromHeader] int solutionId) =>
+            Ok(await Task.Run(() => _db.TestRuns.Where(testRun => testRun.SolutionId == solutionId).Select(testRun => new {testRun.InputData, testRun.OutputData, testRun.ResultRun})));
+    }
+}
