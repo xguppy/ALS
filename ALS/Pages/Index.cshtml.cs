a36ccc7e7f58b55b9ee4330b306a237ba180df7d
Guppy <xguppy@yandex.ru>
2019-07-15 23:30:15 +0400

    Code File to View

---
+++ ALS/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ALS/Pages/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class _Layout : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
