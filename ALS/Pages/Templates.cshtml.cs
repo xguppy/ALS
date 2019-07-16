27fcd6ed772fb5d8ef58623badfba79d8dc959b1
PyatiyEtaj <pyatiyEtaj@gmail.com>
2019-07-16 21:27:45 +0400

    Template to One Page

---
+++ ALS/Pages/Templates.cshtml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ALS.DTO;
+using ALS.EntityСontext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace ALS.Pages
+{
+    public class TemplatesModel : PageModel
+    {
+        public ApplicationContext _context;
+        public List<TemplateLaboratoryWork> Templates;
+        public TemplateLaboratoryWork NewTemplate;
+        public List<SelectListItem> Themes;
+        private IHostingEnvironment _environment;
+        private IHttpClientFactory _clientFactory;
+
+        public TemplatesModel(ApplicationContext context, IHttpClientFactory clientFactory, IHostingEnvironment env)
+        {
+            Templates = new List<TemplateLaboratoryWork>();
+            _context = context;
+            _clientFactory = clientFactory;
+            _environment = env;
+            NewTemplate = new TemplateLaboratoryWork();
+        }
+
+        public async Task OnGet()
+        {
+            Templates = await Task.Run(() => _context.TemplateLaboratoryWorks.Include(x => x.Theme).Select(x => x).ToList());
+            Themes = await Task.Run(() => _context.Themes.Select(x =>
+                     new SelectListItem
+                     {
+                         Value = x.ThemeId.ToString(),
+                         Text = x.Name
+                     }).ToList()
+                 );
+        }
+
+        public async Task<IActionResult> OnPostCreatingAsync(int themeId, IFormFile upload)
+        {
+            var file = Path.Combine(_environment.ContentRootPath, "uploads", upload.FileName);
+
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                await upload.CopyToAsync(fileStream);
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post,
+                "https://localhost:44305/api/TemplateLWS/Create");
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(new TemplateLWDTO() { ThemeId = themeId, TemplateTask = new Uri(file).AbsoluteUri }), Encoding.UTF8, "application/json");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("");
+            }
+            return RedirectToPage("/Error");
+        }
+
+        public async Task<IActionResult> OnPostDeletingAsync(int tlwId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post,
+                "https://localhost:44305/api/TemplateLWS/Delete");
+
+            request.Headers.Add("templateId", tlwId.ToString());
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("./Index");
+            }
+            return RedirectToPage("/Error");
+        }
+
+    }
+}
