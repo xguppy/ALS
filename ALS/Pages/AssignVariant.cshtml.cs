df399b7b3db18d99980782755aa938edf75af86a
Guppy <xguppy@yandex.ru>
2019-08-19 22:15:57 +0400

    New Web UI

---
+++ ALS/Pages/AssignVariant.cshtml.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class AssignVariant : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ALS/Pages/Contest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class Contest : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
