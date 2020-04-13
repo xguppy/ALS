e8ac8f665888e43e8c2998fd60892d4c46d11df9
Guppy <xguppy@yandex.ru>
2020-04-14 02:42:39 +0400

    Add UI For Preparers And Finaliters

---
+++ ALS/Pages/Preparers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class Preparers : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
