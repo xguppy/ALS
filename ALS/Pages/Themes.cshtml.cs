d13f38808428f9fe67786033b267b8d0058188ba
PyatiyEtaj <pyatiyEtaj@gmail.com>
2020-05-11 16:23:37 +0400

    modals - added bootstrap styles

--- ALS/Pages/Themes.cshtml.cs
+++ ALS/Pages/Themes.cshtml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ALS.EntityСontext;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ALS.Pages
 {
@@ -10,6 +11,7 @@ namespace ALS.Pages
     {
         private ApplicationContext _context;
         public List<Theme> Themes;
+        public List<SelectListItem> Disciplines;
         public ThemesModel(ApplicationContext context)
         {
             _context = context;
@@ -18,6 +20,13 @@ namespace ALS.Pages
         public async Task OnGetAsync()
         {
             Themes = await Task.Run(() => _context.Themes.Select(x => x).ToList());
+            Disciplines = await Task.Run(() => _context.Disciplines.Select(x =>
+                     new SelectListItem
+                     {
+                         Value = x.Cipher,
+                         Text = x.Name
+                     }).ToList()
+                 );
         }
     }
 }
