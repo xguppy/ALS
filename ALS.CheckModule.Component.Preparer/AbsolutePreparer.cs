ed78924085f91f73284ab5e3f369959402908de5
Guppy <xguppy@yandex.ru>
2020-06-28 03:14:58 +0400

    Small Page Style Fix And Add Empty Components

---
+++ ALS.CheckModule.Component.Preparer/AbsolutePreparer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.IO;
+using ALS.CheckModule.Compare.Preparer;
+
+namespace ALS.CheckModule.Component.Preparer
+{
+    public class AbsolutePreparer: IPreparer
+    {
+        public void Prepare(string pathToUserProgram, string pathToModelProgram, List<string> input, ref bool isStdInput)
+        {
+
+        }
+
+    }
+}
