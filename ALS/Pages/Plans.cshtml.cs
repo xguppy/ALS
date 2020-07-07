777466d25d77358b772609727aba35c22f385475
Guppy <xguppy@yandex.ru>
2020-07-08 00:52:48 +0400

    Fix Users Page

---
+++ ALS/Pages/Plans.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class Plans : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ALS/Pages/UserAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ALS.Pages
+{
+    public class UserAdmin : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
